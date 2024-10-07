fabdfd34c4cd735593017ab87ff9e5931b12e02a
Akio Hamamura <73171585+AkioHamamura@users.noreply.github.com>
2024-10-06 19:09:24 -0500

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React, { useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   const [image, setImage] = useState(null);
+  const [randomNumber, setRandomNumber] = useState(null);
+
 
   const handleImageUpload = (event) => {
     const file = event.target.files[0];
@@ -16,7 +18,23 @@ function App() {
     }
   };
 
-  return (
+    useEffect(() => {
+        async function fetchRandomNumber() {
+            const number = await getRandomNumber();
+            setRandomNumber(number);
+        }
+        fetchRandomNumber();
+    }, []);
+
+    async function getRandomNumber() {
+        const apiUrl = 'https://zrmulzpv9b.execute-api.us-east-1.amazonaws.com/prod/random';
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        return data.body;
+    }
+
+ return (
+
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
@@ -33,6 +51,8 @@ function App() {
           >
             Learn React
           </a>
+            <p>Random number:</p>
+            <p>{randomNumber}</p>
         </header>
       </div>
   );
